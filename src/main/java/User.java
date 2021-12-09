faea6396818c395b19ff68d6575af0818cbd16f3
曾攀辉 <532339734@qq.com>
2021-12-09 14:30:37 +0800

    Initial commit

---
+++ src/main/java/User.java
@@ -0,0 +1,12 @@
+/**
+ * description TODO
+ *
+ * @author zph
+ * date 2021/12/9 14:29
+ */
+
+public class User {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
